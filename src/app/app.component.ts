557ed41643c7c454f3f9ea7c67f01c614b37eca1
Dirk Schemmel <schemmel@lionsoft.de>
2021-06-09 08:03:07 +0200

    tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
+    <div>
+      <h1 class="m-5 p-5 text-center text-2xl text-blue-600 font-bold bg-blue-200 shadow-md">
         Welcome to {{title}}!
       </h1>
-      <span style="display: block">{{ title }} app is running!</span>
+      <p>{{ title }} app is running!</p>
       <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
     </div>
     <h2>Here are some links to help you start: </h2>
@@ -25,7 +25,31 @@ import { Component } from '@angular/core';
     </ul>
     
   `,
-  styles: []
+  styles: [ `
+    p {
+      @apply text-center;
+    }
+    
+    img {
+      @apply block m-auto h-1/4;
+    }
+    
+    h2 {
+      @apply font-bold;
+    }
+    
+    ul {
+      @apply bg-blue-200;
+    
+        li {
+          @apply ml-10;
+        }
+      
+      a {
+        @apply hover:underline;
+      }
+    }
+  ` ],
 })
 export class AppComponent {
   title = 'ng12tw-b';
